91cc205a5f493996a0d4f75c922e4749e2a3adad
thelvm <l.viegasmilani@gmail.com>
2024-04-21 11:25:42 +0200

    Fixed wrong argument order in snap function usage

--- Scripts/TileMovement.gd
+++ Scripts/TileMovement.gd
@@ -52,13 +52,23 @@ func move(delta: float):
 	if vector2_to_direction_mask(current_direction) & available_directions_mask == 0:
 		current_direction = Vector2.ZERO
 	
-	var can_change_direction: bool = true
-	
-	if can_change_direction:
-		current_direction = next_direction
-		next_direction = Vector2.ZERO
+	if next_direction != Vector2.ZERO:
+		var can_change_direction: bool = true
+		if can_change_direction:
+			current_direction = next_direction
+			next_direction = Vector2.ZERO
 	
 	position += current_direction * speed * delta
+	
+	match current_direction:
+		Vector2.UP:
+			position.x = Util.snap(position.x, tile_size, 0, offset)
+		Vector2.DOWN:
+			position.x = Util.snap(position.x, tile_size, 0, offset)
+		Vector2.LEFT:
+			position.y = Util.snap(position.y, tile_size, 0, offset)
+		Vector2.RIGHT:
+			position.y = Util.snap(position.y, tile_size, 0, offset)
 
 func update_requested_direction(new_requested_direction: Vector2):
 	requested_direction = new_requested_direction
@@ -68,8 +78,16 @@ func update_available_directions_mask():
 	var cell_tile_data: TileData = tile_map.get_cell_tile_data(0, cell_tile_position)
 	available_directions_mask = cell_tile_data.get_custom_data_by_layer_id(0)
 
-func vector2_to_direction_mask(direction: Vector2) -> int:
+func vector2_to_direction_mask(direction: Vector2) -> DirectionMask:
 	match direction:
+		Vector2.UP:
+			return DirectionMask.UP
+		Vector2.DOWN:
+			return DirectionMask.DOWN
+		Vector2.LEFT:
+			return DirectionMask.LEFT
+		Vector2.RIGHT:
+			return DirectionMask.RIGHT
 		_:
 			return DirectionMask.NONE
 
@@ -77,20 +95,20 @@ func next_middle_tile() -> Vector2:
 	var next_middle: Vector2
 	match current_direction:
 		Vector2.UP:
-			next_middle.x = Util.snap(position.x, tile_size, offset, -1)
-			next_middle.y = Util.snap(position.y, tile_size, offset, 0)
+			next_middle.x = Util.snap(position.x, tile_size, 0, offset)
+			next_middle.y = Util.snap(position.y, tile_size, -1, offset)
 		Vector2.DOWN:
-			next_middle.x = Util.snap(position.x, tile_size, offset, 1)
-			next_middle.y = Util.snap(position.y, tile_size, offset, 0)
+			next_middle.x = Util.snap(position.x, tile_size, 0, offset)
+			next_middle.y = Util.snap(position.y, tile_size, 1, offset)
 		Vector2.LEFT:
-			next_middle.x = Util.snap(position.x, tile_size, offset, 0)
-			next_middle.y = Util.snap(position.y, tile_size, offset, -1)
+			next_middle.x = Util.snap(position.x, tile_size, -1, offset)
+			next_middle.y = Util.snap(position.y, tile_size, 0, offset)
 		Vector2.RIGHT:
-			next_middle.x = Util.snap(position.x, tile_size, offset, 0)
-			next_middle.y = Util.snap(position.y, tile_size, offset, 1)
+			next_middle.x = Util.snap(position.x, tile_size, 1, offset)
+			next_middle.y = Util.snap(position.y, tile_size, 0, offset)
 		_:
-			next_middle.x = Util.snap(position.x, tile_size, offset, 0)
-			next_middle.y = Util.snap(position.y, tile_size, offset, 0)
+			next_middle.x = Util.snap(position.x, tile_size, 0, offset)
+			next_middle.y = Util.snap(position.y, tile_size, 0, offset)
 	
 	debug_next_middle_tile_updated.emit(next_middle)
 	
