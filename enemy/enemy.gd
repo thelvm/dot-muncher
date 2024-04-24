6b5755f564ff7153cd6e2a1ebbd59a43a21d5744
thelvm <luckiler3@gmail.com>
2024-04-24 23:49:25 +0300

    Started working on Enemy logic

--- enemy/enemy.gd
+++ enemy/enemy.gd
@@ -1,22 +1,18 @@
-extends TileMovement
+class_name Enemy extends Node2D
 
-@onready var nav_agent: NavigationAgent2D = $NavigationAgent2D
+enum Mode {
+	IDLE,
+	HUNT,
+	SCATTER,
+	COWER
+}
+
 @export var target: Node2D
 
-func _physics_process(delta):
-	var direction = position.direction_to(nav_agent.get_next_path_position())
-	
-	if absf(direction.x) < absf(direction.y):
-		direction.x = 0
-	else:
-		direction.y = 0
-	
-	direction = direction.normalized()
-	
-	if direction == Vector2.UP or direction == Vector2.DOWN:
-		position.x = Util.snap(position.x, 16, 0) - 8
-	elif direction == Vector2.LEFT or direction == Vector2.RIGHT:
-		position.y = Util.snap(position.y, 16, 0) - 8
+var _current_mode: Mode = Mode.IDLE
 
-func update_path():
+@onready var nav_agent: NavigationAgent2D = $NavigationAgent2D
+
+
+func update_target_position():
 	nav_agent.target_position = target.global_position
