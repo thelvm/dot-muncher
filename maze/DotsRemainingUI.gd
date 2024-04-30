ca0d2f1ab818ef997cffe153a554e9a4d28a6c25
thelvm <luckiler3@gmail.com>
2024-04-30 14:01:43 +0200

    Added tweening to score

--- maze/DotsRemainingUI.gd
+++ maze/DotsRemainingUI.gd
@@ -1,4 +1,16 @@
 extends Label
 
+var last_score: int = 0
+var score_display: int = 0
+var score_display_tween: Tween
+
+
 func _process(_delta: float) -> void:
-	text = "Score: " + str(GameManager.score)
+	if last_score != GameManager.score:
+		last_score = GameManager.score
+		if score_display_tween:
+			score_display_tween.kill()
+		score_display_tween = create_tween()
+		score_display_tween.tween_property(self, "score_display", GameManager.score, 0.1)
+	
+	text = "Score: " + str(score_display)
